9033548b5816f9ded0b9e34c988487073d4c08c1
Datbt <buithanhdat15051997@gmail.com>
2023-07-01 16:39:08 +0700

    Add test note

--- app/src/test/java/com/example/data/repository/NoteRepositoryImplTest.kt
+++ app/src/test/java/com/example/data/repository/NoteRepositoryImplTest.kt
@@ -1,13 +1,21 @@
 package com.example.data.repository
 
+import android.util.Log
 import com.example.data.datasource.local.NoteDao
+import com.example.data.datasource.local.NoteEntity
+import com.example.data.models.CurrencyDataDto
+import com.example.data.models.QuotesDto
 import com.example.domain.model.Note
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -30,5 +38,22 @@ internal class NoteRepositoryImplTest {
     @Test
     fun testInsertNote() = runBlocking {
         noteRepositoryImpl.insertNote(Note(title = "title", content = "content", 12343243))
+    }
+
+    @Test
+    fun testGetAllNotes() = runBlocking {
+        `when`(noteDao.getNoteLists()).thenReturn(
+            flow {
+                emit(getNoteDataList())
+            }.flowOn(Dispatchers.IO)
+                .conflate()
+        )
+
+       val allNotes = noteRepositoryImpl.getAllNotes.first()
+        assertNotNull(allNotes)
+    }
+
+    private fun getNoteDataList(): List<NoteEntity> {
+        return arrayListOf(NoteEntity(title = "Title", content = "Content", timestamp = 123098712))
     }
 }
